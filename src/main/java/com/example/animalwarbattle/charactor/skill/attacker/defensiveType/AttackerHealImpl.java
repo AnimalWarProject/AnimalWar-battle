f97d8d48eec4b7f8383d8a8099a77e7bf1f31b08
ptwoj <kauze1998@gamil.com>
2023-10-24 20:57:59 +0900

    #12 Heal skill code implement .

--- src/main/java/com/example/animalwarbattle/charactor/skill/attacker/defensiveType/AttackerHealImpl.java
+++ src/main/java/com/example/animalwarbattle/charactor/skill/attacker/defensiveType/AttackerHealImpl.java
@@ -5,7 +5,7 @@ import com.example.animalwarbattle.charactor.domain.entity.Character;
 import java.util.Random;
 
 /*  힐 : 전투시 잃은 체력의 30%를 회복합니다.  */
-public class AttackerHealImpl implements AttackerDefensiveTypeSkill{
+public class AttackerHealImpl implements AttackerDefensiveTypeSkill {
     @Override
     public void defensiveTypeAttackerExecute(Character attacker, Character defender) {
 
@@ -14,10 +14,19 @@ public class AttackerHealImpl implements AttackerDefensiveTypeSkill{
         Random random = new Random();
         // 1. 비상식량 스킬 발동 설정
         // 잃은 체력
-        int AttackerLostLife = attacker.getMaxLife() - attacker.getLife();
+        int attackerLostLife = attacker.getMaxLife() - attacker.getLife();
         // 공격자 체력 설정
         int attackerLife = attacker.getLife();
         int remainingHealth;
-        // 1-1. 
+        // 1-1. 힐 스킬 발동 설정
+        int attackerHealSkill = (int) ((0.3 * attackerLostLife) + attackerLife);
+
+        // 2. 스킬 발동(확률 발동)
+        if (random.nextDouble() < poisonProbability) {
+            remainingHealth = attackerHealSkill + attackerLife;
+            attacker.setLife(remainingHealth);
+        } else {
+            System.out.println("Heal skill failed");
+        }
     }
 }
--- src/main/java/com/example/animalwarbattle/charactor/skill/defender/defensiveType/DefenderHealImpl.java
+++ src/main/java/com/example/animalwarbattle/charactor/skill/defender/defensiveType/DefenderHealImpl.java
@@ -2,9 +2,31 @@ package com.example.animalwarbattle.charactor.skill.defender.defensiveType;
 
 import com.example.animalwarbattle.charactor.domain.entity.Character;
 
+import java.util.Random;
+
 public class DefenderHealImpl implements DefenderDefensiveTypeSkill{
     @Override
     public void defensiveTypeDefenderExecute(Character attacker, Character defender) {
+
+        // 0. 스킬 확률
+        double poisonProbability = 0.7;
+        Random random = new Random();
+        // 1. 비상식량 스킬 발동 설정
+        // 잃은 체력
+        int defenderLostLife = defender.getMaxLife() - defender.getLife();
+        // 공격자 체력 설정
+        int defenderLife = defender.getLife();
+        int remainingHealth;
+        // 1-1. 힐 스킬 발동 설정
+        int defenderHealSkill = (int) ((0.3 * defenderLostLife) + defenderLife);
+
+        // 2. 스킬 발동(확률 발동)
+        if (random.nextDouble() < poisonProbability) {
+            remainingHealth = defenderHealSkill + defenderLife;
+            defender.setLife(remainingHealth);
+        } else {
+            System.out.println("Heal skill failed");
+        }
 
     }
 }
